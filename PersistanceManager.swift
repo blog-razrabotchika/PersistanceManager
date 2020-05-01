494b6c65ee4e43697c96b0fef1cdaebc6c85d652
Sergey <53207069+blog-razrabotchika@users.noreply.github.com>
2020-05-01 11:31:59 +0300

    init

--- PersistanceManager.swift
+++ PersistanceManager.swift
@@ -9,7 +9,7 @@ final class PersistanceManager {
     
     lazy var persistentContainer: NSPersistentContainer = {
        
-        let persistentContainer = NSPersistentCloudKitContainer(name: "NAME")
+        let persistentContainer = NSPersistentContainer(name: "DBName")
         persistentContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                
@@ -25,7 +25,6 @@ final class PersistanceManager {
 
     func saveContext () {
         let context = persistentContainer.viewContext
-        context.automaticallyMergesChangesFromParent = true
         if context.hasChanges {
             do {
                 try context.save()
@@ -36,12 +35,27 @@ final class PersistanceManager {
         }
     }
     
+    func resetAllRecords(in entity : String)
+    {
+        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
+        do
+        {
+            try context.execute(deleteRequest)
+            try context.save()
+        }
+        catch
+        {
+            print ("Error")
+        }
+    }
+    
     func fetch<T: NSManagedObject>(_ objectType: T.Type) -> [T] {
-        
+
         let entityName = String(describing: objectType)
-        
+
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
-        
+
         do {
             let fetchedObjects = try context.fetch(fetchRequest) as? [T]
             return fetchedObjects ?? [T]()
@@ -51,22 +65,29 @@ final class PersistanceManager {
         }
     }
 
-    func buildFRC<T:NSManagedObject>(managedObjectContext:NSManagedObjectContext, entity: T.Type, sortKey: String) -> NSFetchedResultsController<T>? {
+    func buildFRC<T:NSManagedObject>(managedObjectContext:NSManagedObjectContext, entity: T.Type, sortKey: String, sections: String?) -> NSFetchedResultsController<T>? {
         let fetchedResultController: NSFetchedResultsController<T>
                   
         let request: NSFetchRequest<T> = T.fetchRequest() as! NSFetchRequest<T>
-                  let sort = NSSortDescriptor(key: sortKey, ascending: true)
-                  request.sortDescriptors = [sort]
+                let sort = NSSortDescriptor(key: sortKey, ascending: false)
+                request.sortDescriptors = [sort]
+                #if !targetEnvironment(macCatalyst)
+                    request.fetchBatchSize = 10;
+                    request.fetchLimit = 10;
+                #endif
+                
+                request.includesPendingChanges = true
                   
-        fetchedResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
-                  
+        fetchedResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: managedObjectContext, sectionNameKeyPath: sections, cacheName: nil)
+        
                   do {
                       try fetchedResultController.performFetch()
                   }
                   catch {
-                      fatalError("Error in fetching records")
+                      fatalError("Fetching records error")
                   }
                   
         return fetchedResultController
               }
     }
+
